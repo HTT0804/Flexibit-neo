18758e28cd7d4d21be0343115e755815c91db29f
Ramond <patrickchang@hightt.com>
2022-08-09 11:12:50 +0800

    Update init.ts

--- init.ts
+++ init.ts
@@ -18,6 +18,8 @@ namespace Flexibit {
         let i2c_id = 40
         let dr4 = 0x2008 /* send 32(dex) to reg A8 */
         let dr10 = 0x2402 /* send 36(dex) to reg A2 */
+        let dr11 = 0x2400 /* send 36(dex) to reg A2 */
+
         pins.i2cWriteNumber(
             i2c_id,
             dr4,
